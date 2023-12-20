def3ec4007dec7cba125ef376b33e01ff06abe02
Mitu <iammituraj@gmail.com>
2023-12-20 12:15:11 +0530

    Added packages and includes

--- src/common/pqr5_core_pkg.sv
+++ src/common/pqr5_core_pkg.sv
@@ -110,6 +110,194 @@ localparam [3:0] ALU_SRL  = 4'b1010 ;
 localparam [3:0] ALU_SRA  = 4'b1011 ;
 localparam [3:0] ALU_ILLG = 4'b1111 ;  // Illegal opcode
 
+//===================================================================================================================================================
+// Functions/Tasks
+//===================================================================================================================================================
+// Task to display simulation header
+task disp_simheader ();
+   $display("");
+   $display("//////////////////////////////////////////////////////////////////");
+   $display("//  chipmunklogic.com                      O P E N S O U R C E  //"); 
+   $display("//////////////////////////////////////////////////////////////////");    
+   $display("// +==========================================================+ //"); 
+   $display("// |              Pequeno RISC-V CPU - Simulation             | //");
+   $display("// +==========================================================+ //");
+   $display("// | Version   : %-044s | //", `VERS);
+   $display("// | ISA       : %-044s | //", `ISA);
+   $display("// | PC_INIT   : %-044s | //", rhex2txt(32, `PC_INIT));
+   `ifdef DBG
+   $display("// | DBG       : %-044s | //", "YES");
+   `else
+   $display("// | DBG       : %-044s | //", "NO");
+   `endif
+   `ifdef MEM_DBG
+   $display("// | MEM_DBG   : %-044s | //", "YES");
+   `else
+   $display("// | MEM_DBG   : %-044s | //", "NO");
+   `endif
+   $display("// | REGF dump : %-044s | //", ynstatus(`REGFILE_DUMP));
+   $display("// | IMEM dump : %-044s | //", ynstatus(`IMEM_DUMP));
+   $display("// | DMEM dump : %-044s | //", ynstatus(`DMEM_DUMP));
+   `ifdef SIMLIMIT
+   $display("// | SIMLIMIT  : %-044s | //", "YES");
+   `else
+   $display("// | SIMLIMIT  : %-044s | //", "NO");
+   `endif
+   $display("// +==========================================================+ //");
+   $display("//////////////////////////////////////////////////////////////////");
+   $display(""); 
+endtask
+
+// Function to convert hex to text for printing in file: [prefix][data with separator][suffix]
+function automatic void hex2txtf (int fptr, int size, logic [DSIZE-1:0] hexval, string prefix = "32'h", string sep = "_", int sepnib = 4, string suffix = "");
+   // Variables
+   int nibbles = (size/4) + (size%4 != 0) ;
+   int zp_size = nibbles * 4 ;
+   int msb     = zp_size - 1 ;
+   int i, j ;
+   string hexstr = "x" ;
+   
+   // Iterate thru each nibble and print   
+   $fwrite(fptr, "%0s", prefix);    // Print prefix
+   for (i=msb, j=1; i>=3; i-=4, j+=1) begin
+       logic x_check = ^hexval[i-:4] ;
+       if (x_check !== 1'bx) hexstr.hextoa(hexval[i-:4]);              
+       $fwrite(fptr, "%0s", hexstr.toupper());                      // Print data in HEX string   
+       if ((j%sepnib == 0) && (i != 3)) $fwrite(fptr, "%0s", sep);  // Print separator
+   end
+   $fwrite(fptr, "%0s", suffix);  // Print suffix
+endfunction
+
+// Function to convert hex to text for printing in stdio [prefix][data with separator][suffix]
+function automatic void hex2txt (int size, logic [DSIZE-1:0] hexval, string prefix = "32'h", string sep = "_", int sepnib = 4, string suffix = "");
+   // Variables
+   int nibbles = (size/4) + (size%4 != 0) ;
+   int zp_size = nibbles * 4 ;
+   int msb     = zp_size - 1 ;
+   int i, j ;
+   string hexstr = "x" ;
+   
+   // Iterate thru each nibble and print
+   $write("%0s", prefix);  // Print prefix
+   for (i=msb, j=1; i>=3; i-=4, j+=1) begin
+         logic x_check = ^hexval[i-:4] ;
+         if (x_check !== 1'bx) hexstr.hextoa(hexval[i-:4]);
+   	   $write("%0s", hexstr.toupper());                      // Print data in HEX string          
+   	   if ((j%sepnib == 0) && (i != 3)) $write("%0s", sep);  // Print separator
+   end
+   $write("%0s", suffix);  // Print suffix
+endfunction
+
+// Function to convert hex to text and return string [prefix][data with separator][suffix]
+function automatic string rhex2txt (int size, logic [DSIZE-1:0] hexval, string prefix = "32'h", string sep = "_", int sepnib = 4, string suffix = "");
+   // Variables
+   int nibbles = (size/4) + (size%4 != 0) ;
+   int zp_size = nibbles * 4 ;
+   int msb     = zp_size - 1 ;
+   int i, j ;
+   string hexstr = "x" ;
+   string txtstr = prefix ;
+   
+   // Iterate thru each nibble and append
+   for (i=msb, j=1; i>=3; i-=4, j+=1) begin
+         logic x_check = ^hexval[i-:4] ;
+         if (x_check !== 1'bx) hexstr.hextoa(hexval[i-:4]);
+         txtstr = {txtstr, hexstr.toupper()} ;  // Append nibble         
+         if ((j%sepnib == 0) && (i != 3)) txtstr = {txtstr, sep} ;  // Append separator
+   end
+   return {txtstr, suffix} ;  // Append suffix and return
+endfunction
+
+// Function to dump Register File
+function automatic void dump_regfile (int fdump, int n, logic [RSIZE-1:0] regarray [], string dumpname);      
+   $fdisplay(fdump, "+======================================");
+   $fdisplay(fdump, "| Pequeno RISC-V CPU v1.0 Simulation   ");
+   $fdisplay(fdump, "+======================================");
+   $fdisplay(fdump, "| %0s", dumpname);
+   $fdisplay(fdump, "+--------------------------------------");
+   for (int r=0; r<n; r++) begin       
+       $fwrite(fdump, "| R%-2d", r, " : ");  // Print reg ID
+       hex2txtf(fdump, RSIZE, regarray[r]);  // Print reg data
+       $fwrite(fdump, "\n");
+   end
+   $fdisplay(fdump, "+======================================");   
+endfunction
+
+// Function to display Register File
+function automatic void disp_regfile (logic [RSIZE-1:0] regarray []);
+   int i = 0 ;
+   $display("+================================================================+");
+   $display("| REGFILE                                                        |");
+   $display("+================================================================+");
+   $write  ("| R0-3   :");
+   while (i<4) begin hex2txt (32, regarray[i], " 0x", "_", 4, " |"); i=i+1 ; end 
+   $write  ("\n");
+   $write  ("| R4-7   :");
+   while (i<8) begin hex2txt (32, regarray[i], " 0x", "_", 4, " |"); i=i+1 ; end
+   $write  ("\n");
+   $write  ("| R8-11  :");
+   while (i<12) begin hex2txt (32, regarray[i], " 0x", "_", 4, " |"); i=i+1 ; end
+   $write  ("\n");
+   $write  ("| R12-15 :");
+   while (i<16) begin hex2txt (32, regarray[i], " 0x", "_", 4, " |"); i=i+1 ; end
+   $write  ("\n");
+   $write  ("| R16-19 :");
+   while (i<20) begin hex2txt (32, regarray[i], " 0x", "_", 4, " |"); i=i+1 ; end
+   $write  ("\n");
+   $write  ("| R20-23 :");
+   while (i<24) begin hex2txt (32, regarray[i], " 0x", "_", 4, " |"); i=i+1 ; end
+   $write  ("\n");
+   $write  ("| R24-27 :");
+   while (i<28) begin hex2txt (32, regarray[i], " 0x", "_", 4, " |"); i=i+1 ; end
+   $write  ("\n");
+   $write  ("| R28-31 :");
+   while (i<32) begin hex2txt (32, regarray[i], " 0x", "_", 4, " |"); i=i+1 ; end
+   $write  ("\n");
+   $display("+================================================================+");
+endfunction
+
+// Function to return YES/NO/-/NA
+function automatic string ynstatus (bit [1:0] val, string ystr = "YES", string nstr = "NO");
+   if      (val == 2'b00) return nstr  ;
+   else if (val == 2'b01) return ystr  ;
+   else if (val == 2'b10) return "--"  ;
+   else                   return "NA"  ;
+endfunction
+
+// Function to return instruction type
+function automatic string instrtype (bit [5:0] instr_type, logic valid);
+   if (valid) begin
+      if      (instr_type [5]) return "R-type, ALU"    ;
+      else if (instr_type [4]) return "I-type"         ;
+      else if (instr_type [3]) return "S-type, STORE"  ;
+      else if (instr_type [2]) return "B-type, BRANCH" ;
+      else if (instr_type [1]) return "U-type"         ;
+      else if (instr_type [0]) return "J-type, JAL"    ;
+   end
+   else begin
+      return "--" ; 
+   end
+endfunction
+
+// Function to return memory access type
+function automatic string memacctype (logic[1:0] size, logic cmd, logic valid);
+   if (valid) begin
+      if (cmd) begin
+         if      (size == 2'b00) return "8-bit, Store"  ;   
+         else if (size == 2'b01) return "16-bit, Store" ;  
+         else                    return "32-bit, Store" ;   
+      end
+      else begin
+         if      (size == 2'b00) return "8-bit, Load"   ;   
+         else if (size == 2'b01) return "16-bit, Load"  ;  
+         else                    return "32-bit, Load"  ;   
+      end
+   end
+   else begin
+      return "--" ;
+   end
+endfunction
+
 endpackage
 //###################################################################################################################################################
 //                                                         P Q R 5   C O R E   P A C K A G E                                         
