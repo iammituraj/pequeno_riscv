8801d1c32c32314ea9bda3e65a8aeb00ba370f3d
Mitu <iammituraj@gmail.com>
2025-09-30 22:43:13 +0530

    Updated comments of macros

--- src/include/pqr5_core_macros.svh
+++ src/include/pqr5_core_macros.svh
@@ -56,16 +56,23 @@
 `define INSTR_END     32'hEEE0_0013        // END sim instruction (mvi x0, 0xEEE); known only by sim framework, NOT an Assembler instruction
 //---------------------------------------------------------------------------------------------------------------------------------------------------
 
-//---------------------------------------------------------------------------------------------------------------------------------------------------
+//===================================================================================================================================================
 // Configurable macros
-//---------------------------------------------------------------------------------------------------------------------------------------------------
+//===================================================================================================================================================
+// Sample configurations:
+// ----------------------
+// Light         = 
+// Standard      = RAS
+// Performance   = BPREDICT_DYN
+// Performance++ = BPREDICT_DYN + RAS
+//====================================================================================================================================================
 // PC 
 `define PC_INIT       32'h0000_0000       // PC init address after CPU reset i.e., the reset vector (32-bit aligned address)
 //`define RF_ON_BRAM                        // Define this macro to generate Block RAM based Register File, 
                                           // Else Flops/LUT RAM based Register File is generated
 
 // Branch Predictor & RAS
-`define BPREDICT_DYN                      // Define this macro to generate Pequeno GShare Dynamic Branch Predictor
+`define BPREDICT_DYN                      // Define this macro to generate Pequeno GShare Dynamic Branch Predictor, else Static predictor
                                           // Else generate Static Branch Predictor (backward always taken strategy)
 `define BHT_IDW           10              // BHT index width; for eg: 6 = 2^6 = 64 entries of 2-bit. This macro is qualified by IS_BPREDICT_DYN = 1
 `define BHT_TYPE          "lutram"        // Branch History Table (BHT) target configuration. This macro is qualified by IS_BPREDICT_DYN = 1
@@ -76,7 +83,7 @@
 `define GHRW              (`BHT_IDW+2)    // Global History Register (GHR) width
 
 `define RAS                               // Define this macro to generate RAS (Return Address Stack) predictor
-`define RAS_DPT           8               // RAS depth, size = (N x 32) bits
+`define RAS_DPT           8               // RAS depth, size = (N x 32) bits; Depth must be 2^N
 
 // Test & Debug
 //`define TEST_PORTS                        // Define this macro to generate test ports from the core: x31 bits, boot flag
@@ -85,7 +92,7 @@
 //`define CORE_SYNTH                        // Define this macro to configure the core for SYNTHESIS
 
 // Simulation related
-`define DBG                               // Define this macro to generate all Debug interfaces                        ; OVERRIDEN FOR SYNTHESIS
+`define DBG                               // Define this macro to generate all Debug interfaces, and display performance summary ; OVERRIDEN FOR SYNTHESIS
 //`define DBG_PRINT                         // If DBG is enabled: Define this macro to display per-cycle debug messages  ; OVERRIDEN FOR SYNTHESIS
 `define SIMEXIT_INSTR_END                 // Define this macro to exit simulation on receiving END sim instruction     ; OVERRIDEN FOR SYNTHESIS
 `define REGFILE_DUMP  1                   // If DBG is enabled: '1'- Dump Register File @end of sim, '0'- No dump      ; OVERRIDEN to 0 for SYNTHESIS
